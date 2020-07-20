22401ba4d9b8e7ceec0547d110ae29e16709ee03
Michell Cronberg <michell@cronberg.dk>
2020-07-20 22:00:25 +0200

    v1

--- WebApiDemo/Startup.cs
+++ WebApiDemo/Startup.cs
@@ -26,6 +26,7 @@ namespace WebApiDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<SQLiteEF.PeopleContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
